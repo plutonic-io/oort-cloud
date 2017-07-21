6093ad906f937cdce246e053064e14b233c11e38
Jeremy Malczyk <jmalczyk@gmail.com>
2017-07-21 13:50:38 -0400

    WIP CLI mangles MB-GL in production mode

--- src/app/pl-map-gl/map/map.component.ts
+++ src/app/pl-map-gl/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import mapboxgl from 'mapbox-gl';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'pl-map-gl',
@@ -13,7 +13,9 @@ export class MapGlComponent implements OnInit {
   constructor() { }
   ngOnInit() {
     try {
-      mapboxgl.accessToken = 'pk.eyJ1Ijoiam1hbGN6eWsiLCJhIjoiY2lxeXBrbzl5MDF0ZGZ2bThhM2UyYjRnNyJ9.TKceYTp5hwWY-WuHjs_15w';
+      Object.getOwnPropertyDescriptor(
+        mapboxgl, 'accessToken').set(
+          'pk.eyJ1Ijoiam1hbGN6eWsiLCJhIjoiY2lxeXBrbzl5MDF0ZGZ2bThhM2UyYjRnNyJ9.TKceYTp5hwWY-WuHjs_15w');
       const map = new mapboxgl.Map({
           container: this.node.nativeElement,
           style: 'mapbox://styles/mapbox/streets-v9'
